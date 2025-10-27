14e12cfde044fade3792e5e7ab04b4d06dfeac07
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-10-26 18:25:50 -0600

    IDK WTF I'm doing

--- flake.nix
+++ flake.nix
@@ -1,47 +1,29 @@
 {
-  description = "My Nix Config";
-
-  nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-		nix-gaming.url = "github:fufexan/nix-gaming";
+
+    wrappers.url = "github:Lassulus/wrappers/39b27c1bbf6cfc38afb570f98664540639fc52f8";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # impermanence.url = "github:nix-community/impermanence";
+    # persist-retro.url = "github:Geometer1729/persist-retro";
+
+    # disko = {
+    #   url = "github:nix-community/disko";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    #
+    hjem = {
+      url = "github:feel-co/hjem";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , home-manager
-    , ...
-    } @ inputs:
-    {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-						./hosts/nixos/configuration.nix
-					];
-          # specialArgs = { inherit inputs outputs; };
-        };
-      };
-
-      homeConfigurations = {
-        nixos = inputs.home-manager.lib.homeManagerConfiguration {
-					pkgs = import nixpkgs {
-						system = "x86_64-linux";
-						config.allowUnfree = true;
-					};
-					extraSpecialArgs = { inherit inputs; };
-          modules = [
-						./hosts/nixos/home.nix
-					];
-          # pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          # extraSpecialArgs = { inherit inputs outputs; };
-        };
-      };
-    };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 }
-
