c2edcdda82b4ecc6b42f890dc1b01e1debf86c08
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-10-27 12:51:39 -0600

    IDK WTF I'm doing

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      <home-manager/nixos>
+			# <home-manager/nixos>
     ];
 
   boot.loader.systemd-boot.enable = true;
