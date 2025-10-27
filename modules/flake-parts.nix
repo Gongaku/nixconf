9aa2345de8fabc9a073992f7dfd37d6ff587cf65
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-10-26 18:47:58 -0600

    IDK WTF I'm doing

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -1,12 +1,13 @@
 {inputs, ...}: {
-	imports = [
-		inputs.flake-parts.flakeModules.modules
-	];
+  imports = [
+    # currently unused
+    inputs.flake-parts.flakeModules.modules
+  ];
 
-	systems = [
-		"aarch64-darwin"
-		"aarch64-linux"
-		"x86_64-darwin"
-		"x86_64-linux"
-	];
+  systems = [
+    "aarch64-darwin"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "x86_64-linux"
+  ];
 }
